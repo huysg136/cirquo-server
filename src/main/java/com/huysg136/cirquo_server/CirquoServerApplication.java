ea369027269691aeef68e05211af284321953438
huysg136 <thaigiahuy6912@gmail.com>
2026-08-30 12:15:38 +0700

    Initial commit

---
+++ src/main/java/com/huysg136/cirquo_server/CirquoServerApplication.java
@@ -0,0 +1,13 @@
+package com.huysg136.cirquo_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CirquoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CirquoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huysg136/cirquo_server/CirquoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huysg136.cirquo_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CirquoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
