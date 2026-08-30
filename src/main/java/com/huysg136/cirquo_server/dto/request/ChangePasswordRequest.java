56805da9034e005f24842faa583b10d483b67068
huysg136 <thaigiahuy6912@gmail.com>
2026-08-31 01:18:30 +0700

    refactor: reorganize from layer to feature packages

--- src/main/java/com/huysg136/cirquo_server/dto/request/ChangePasswordRequest.java
+++
@@ -1,6 +0,0 @@
-package com.huysg136.cirquo_server.dto.request;
-
-public class ChangePasswordRequest {
-    private String currentPassword;
-    private String newPassword;
-}
--- src/main/java/com/huysg136/cirquo_server/enums/UserStatus.java
+++
@@ -1,6 +0,0 @@
-package com.huysg136.cirquo_server.enums;
-
-public enum UserStatus {
-    ACTIVE,
-    BLOCKED
-}
